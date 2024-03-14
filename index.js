1a7a83e9c59326569d3f42232f83ca9049382bd3
Joseph Daza <josephdazz@hotmail.com>
2024-03-14 13:59:41 -0400

    index.js updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function menu() {
             type: "list",
             message: "What would you like to do?"  
       name: "option"
-      choices: [view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role]
+      choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
 
         }
     ])
@@ -32,21 +32,33 @@ function menu() {
     if (response.option === "view all employees") {
         viewAllEmployees()
     }
+    else if (response.option === "view all roles") {
+        viewAllRoles();
+    }
 })
+function viewAllRoles() {
+    db.query(`SELECT role.id as id, title, name as department, salary FROM role 
+    LEFT JOIN department
+    ON role.department_id=department.id;`, (err, data) => {
+        printTable(data)
+        menu()
+    })
 
-function viewAllEmployees(){
-      db.query(`SELECT employee.id as id, first_name, last_name, title, 
+}
+
+function viewAllEmployees() {
+    db.query(`SELECT employee.id as id, first_name, last_name, title, 
       name as department, salary, 
       CONCAT(managerTable.first_name, ' ',managerTable.last_name) as manager
       FROM employee 
       LEFT JOIN role ON employee.role_id = role_id 
       LEFT JOIN department ON role.department_id=department.id
       LEFT JOIN employee as managerTable 
-      ON employee.manager_id=managerTable.id;`, (err,data)=>{
+      ON employee.manager_id=managerTable.id;`, (err, data) => {
 
         printTable(data)
         menu()
 
-      })
+    })
 
 }
