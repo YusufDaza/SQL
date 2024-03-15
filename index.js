37627ae1daa1e13aabb2c1bc012c084a339023e3
Joseph Daza <josephdazz@hotmail.com>
2024-03-15 14:02:01 -0400

    index.js updated

--- index.js
+++ index.js
@@ -20,45 +20,40 @@ function menu() {
     inquirer.prompt([
         {
             type: "list",
-            message: "What would you like to do?"  
-      name: "option"
-      choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+            message: "What would you like to do?",
+            name: "option",
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
 
         }
     ])
+        .then(response => {
+            if (response.option === "view all employees") {
+                viewAllEmployees()
+            }
+            else if (response.option === "view all roles") {
+                viewAllRoles();
+            }
+            else if (response.option === "add an employee") {
+                addEmployee()
+            }
+            else if (response.option === "update an employee role") {
 
-}
-.then(response => {
-    if (response.option === "view all employees") {
-        viewAllEmployees()
-    }
-    else if (response.option === "view all roles") {
-        viewAllRoles();
-    }
-})
-function viewAllRoles() {
-    db.query(`SELECT role.id as id, title, name as department, salary FROM role 
-    LEFT JOIN department
-    ON role.department_id=department.id;`, (err, data) => {
-        printTable(data)
-        menu()
-    })
+            }
+        })
+    }        
 
-}
+    function updateEmployee() {
 
-function viewAllEmployees() {
-    db.query(`SELECT employee.id as id, first_name, last_name, title, 
-      name as department, salary, 
-      CONCAT(managerTable.first_name, ' ',managerTable.last_name) as manager
-      FROM employee 
-      LEFT JOIN role ON employee.role_id = role_id 
-      LEFT JOIN department ON role.department_id=department.id
-      LEFT JOIN employee as managerTable 
-      ON employee.manager_id=managerTable.id;`, (err, data) => {
+        db.query(`SELECT id as value, CONCAT(first_name,' ', last_name) as name from employee`, (err,employeeData) => {
+           inquirer.prompt([
 
-        printTable(data)
-        menu()
+            {
+                type: "list", 
+                message: "What is new title for the employee?",
+                name: "employee_id", 
+                choice: employeeData 
+            }
+            ])
 
-    })
 
-}
+        }
